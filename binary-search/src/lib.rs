5ab162e1e404815360b9c3468397ac0dfa9fb0e1
Rob Williams <rwilliams@spotify.com>
2020-07-05 17:24:27 -0400

    implement nice generic version of binary_search

--- binary-search/src/lib.rs
+++ binary-search/src/lib.rs
@@ -1,25 +1,14 @@
-fn subfind(array: &[i32], key: i32, left: usize, right: usize) -> Option<usize> {
-    if right < left {
+pub fn find<T: Ord, U: AsRef<[T]>>(arr: U, key: T) -> Option<usize> {
+    let array = arr.as_ref();
+    if array.is_empty() {
         return None;
     }
-    let mid = (right - left) / 2 + left;
-    eprintln!("left = {}, right = {}, mid = {}", left, right, mid);
+    let mid = array.len() / 2;
     if array[mid] == key {
         return Some(mid);
     }
     if array[mid] > key {
-        if mid == 0 {
-            // prevent trying to put negative in usize (mid-1 below)
-            return None;
-        }
-        return subfind(array, key, left, mid - 1);
+        return find(array.get(..mid).unwrap(), key);
     }
-    return subfind(array, key, mid + 1, right);
-}
-
-pub fn find(array: &[i32], key: i32) -> Option<usize> {
-    if array.is_empty() {
-        return None;
-    }
-    subfind(array, key, 0, array.len() - 1)
+    find(array.get(mid + 1..).unwrap(), key).map(|n| n + mid + 1)
 }
